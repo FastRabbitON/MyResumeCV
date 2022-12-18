9b8605d6be2fbf987edd91b5b9b900968f0dbbb6
FastRabbitON <fastrabbiton@gmail.com>
2022-12-18 15:10:22 +0100

    Main Code

---
+++ script-projects.js
@@ -0,0 +1,12 @@
+
+ProjectPJM = function(){
+    window.open("Projects/Translator/index.html", '_blank').focus();
+}
+
+ProjectKraft = function(){
+    window.open("https://meblekraft.com.pl", '_blank').focus();
+}
+
+ProjectSayColor = function(){
+    window.open("Projects/SayColor/index.html", '_blank').focus();
+}
